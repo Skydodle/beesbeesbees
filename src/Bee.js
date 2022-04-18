12bda97c59ac34de834fee4203503f222fd1d3cd
skydodle <skydodle@gmail.com>
2022-04-18 12:51:38 -0700

    passed all tests in beesbeesbees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,20 @@
 var Bee = function() {
+  Grub.call(this); // set props by Bee
+
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+
 };
+
+Bee.prototype = Object.create(Grub.prototype); // set inherit from Grub methods
+Bee.prototype.constructor = Bee;
+// call the Grub super calss
+// set the prototype
+// set the constructor
+// an age property that is set to 5
+// a color property that is set to yellow
+// a food property that is inherited from grub
+// an eat method that is inherited from grub
+// a job property that is set to keep on growing
 
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,19 @@
 var Grub = function() {
+  // set instance equals {};
+  // instance.age = 0;
+  // instance.color = 'pink';
+  // instance.food = 'jelly';
+  // instance.eat = function() {};
+  // rtn instance;
+
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
+
+Grub.prototype.eat = function() {
+
+};
+
+
 
